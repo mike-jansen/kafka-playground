fc161b27aa45fa1738f1d1ba7e053f5c5e96e293
Mike Jansen <mikejansen@example.com>
2025-08-24 15:09:00 -0700

    update consumer to report throughput metric (596 messages/second)

--- consumer.py
+++ consumer.py
@@ -1,4 +1,6 @@
 from confluent_kafka import Consumer
+import json
+import time
 
 conf = {
     'bootstrap.servers': 'localhost:9092',# specify which kafka broker to connect to
@@ -7,16 +9,40 @@ conf = {
 }
 
 consumer = Consumer(conf)
-consumer.subscribe(['test-topic'])  # producer produces to 'test-topic'
+consumer.subscribe(['crypto-tickers'])  # producer produces to topic
+
+message_count = 0
+start_time = time.time()
+
+
+def report_price(data):
+    print(f"Received message: {data['symbol']} - Price: {data['price']}")
 
 try:
     while True:
-        msg = consumer.poll(0.0)  # wait up to 1 second
+        msg = consumer.poll(1.0)  # wait up to 1 second
         if msg is None:
             continue
         if msg.error():
             print('Error:', msg.error())
             continue
-        print('Received:', msg.value().decode('utf-8'))
+
+        data = json.loads(msg.value().decode('utf-8'))
+        message_count += 1
+
+
+        if data['symbol'] == 'BTCUSDT' or data['symbol'] == 'ETHUSDT' or data['symbol'] == 'XRPUSDT':
+            report_price(data)
+
+        # if message_count % 50 == 0:
+        #     print(f"{data['symbol']}: {data['price']}")
+        #     print()
+
+        # Optional: print throughput every 5 seconds
+        if time.time() - start_time >= 5:
+            print(f"Consumed {message_count} messages in last 5 seconds")
+            message_count = 0
+            start_time = time.time()
+
 finally:
     consumer.close()
