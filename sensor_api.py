f89cea58da34ea7798e2bab08d187f978f6c9e7d
Mike Jansen <mikejansen@example.com>
2025-09-12 13:16:29 -0700

    add FastAPI functionality to make the API callable

--- sensor_api.py
+++ sensor_api.py
@@ -1,3 +1,6 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uvicorn
 import threading
 import random
 import time
@@ -19,4 +22,22 @@ def update_sensor_data():
         sensor_data["humidity"] = max(min(sensor_data["humidity"], 100), 0)
         sensor_data["pressure"] = max(min(sensor_data["pressure"], 1050), 950)
 
-        time.sleep(0.5)
+        time.sleep(0.3)
+
+app = FastAPI()
+
+@app.get("/sensor")  # decorator tells API to call this function if user makes GET request to /sensor
+def get_sensor_data(sensor: str):
+    if sensor:
+        sensor = sensor.lower()
+        if sensor in sensor_data:
+            return JSONResponse(content={sensor: sensor_data[sensor]})
+        else:
+            return JSONResponse(content={"error": "Invalid sensor name"}, status_code=400)
+    return JSONResponse(content=sensor_data)  # return all data if no type was specified
+    
+if __name__ == "__main__":
+    thread = threading.Thread(target=update_sensor_data, daemon=True)
+    thread.start()
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # run server to host the API application
